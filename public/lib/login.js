827c25be121647e1be4d0e904159898fbe221709
GOPI <gopi@koneksahealth.com>
2014-11-17 16:23:48 -0500

    login.js validate passwd

---
+++ public/lib/login.js
@@ -0,0 +1,14 @@
+document.getElementById('passwd').addEventListener('keydown', checkPasswd);
+
+function checkPasswd(event) {
+    
+    var passwd = document.getElementById('passwd');
+
+    var re =  /\w{6,}/;
+
+    console.log(passwd.value);
+
+    console.log(re.test(passwd.value));
+    if(!re.test(passwd.value)) passwd.className = 'red'; 
+    else passwd.className = 'blue';
+}
